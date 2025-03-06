be951b07314d01ce22160845cce26b0857324041
Alfradu <alfred.radu@gmail.com>
2025-03-06 08:55:21 +0000

    added testing mode n cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import strangeDevice from './assets/strange-device.png'
-import './App.css'
+import './css/App.css'
 import Hitsplat from './hitsplat.tsx'
-import haversineDistanceKM from './utils.tsx'
+import haversineDistanceKM from './utils/utils.tsx'
+import { geoGoal1, geoGoal2, geoTest } from './constants.tsx'
 
 function App() {
-  const geoGoal1 = {
-    latitude: 59.8604189,
-    longitude: 17.6034489
-  }
-  const geoGoal2 = {
-    latitude: 59.861524,
-    longitude: 17.603079
-  }
   const [geoLocation, setGeoLocation] = useState({
     latitude: 0,
     longitude: 0
   })
-
+  const [testing, setTesting] = useState(true);
   const [goal, setGoal] = useState({ latitude: 0, longitude: 0 });
   const [goalDistance, setGoalDistance] = useState(0);
   const [health, setHealth] = useState(78);
@@ -28,36 +21,48 @@ function App() {
     y: 0
   });
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (health < 78 && !takingDmg) setHealth(health + 1);
+    }, 5 * 1000);
+  
+    return () => {
+      clearInterval(interval);
+    };
+  }, [health, takingDmg]);
+
   const HandleUpdateDamage = useCallback(() => {
     setTakingDmg(true);
     var currDmg = Math.floor(Math.random() * 10) + 1
     setDamage(currDmg);
-    setHealth((health) => health - currDmg);
+    setHealth(health - currDmg);
     setHitsplatPosition({
       x: (Math.random() * 300) + window.innerWidth / 2 - 150,
       y: (Math.random() * 300) + window.innerHeight / 2 - 150
     });
-    setTimeout(() => {
-      setTakingDmg(false);
-    }, 500);
-
-    if (navigator.geolocation) {
+    if (navigator.geolocation && !testing) {
       navigator.geolocation.getCurrentPosition((position: { coords: { latitude: number; longitude: number; }; }) => {
         setGeoLocation({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         })
-        //TODO: initial haversine calc is wrong?? fix
         setGoalDistance(haversineDistanceKM(position.coords.latitude, position.coords.longitude, goal.latitude, goal.longitude))
       }, () => console.log("Unable to retrieve your location"));
     } else {
-      console.log("Geolocation not supported");
+      setGeoLocation({
+        latitude: geoTest.latitude,
+        longitude: geoTest.longitude
+      })
+      setGoalDistance(haversineDistanceKM(geoTest.latitude, geoTest.longitude, goal.latitude, goal.longitude))
     }
-  }, [health]);
+    setTimeout(() => {
+      setTakingDmg(false);
+    }, 500);
+  }, [health, goal, testing, geoTest]);
 
   const HandleUpdateGoal = useCallback(() => {
     const element = document.getElementById('input') as HTMLInputElement;
-    switch (element.value) {
+    switch (element.value.toLowerCase()) {
       case ("goal1"):
         setGoal(geoGoal1)
         break;
@@ -74,15 +79,16 @@ function App() {
 
   return (
     <>
+      <input type="checkbox" id="testing" name="testing" checked={testing} onChange={() => setTesting(!testing)}></input>
       <input id="input" onChange={HandleUpdateGoal}></input>
-      <br></br>
-      <span>pos Lat {geoLocation.latitude} ,pos Long {geoLocation.longitude}</span>
-      <br></br>
-      <span>goal Lat {goal.latitude} ,goal Long {goal.longitude}</span>
-      <br></br>
-      <span>Distance: {goalDistance} m</span>
+      {testing &&
+        <><br></br><span>pos Lat {geoLocation.latitude} ,pos Long {geoLocation.longitude}</span>
+          <br></br><span>goal Lat {goal.latitude} ,goal Long {goal.longitude}</span>
+          <br></br><span>Distance: {goalDistance} m</span></>
+      }
       <h1>Strange Device</h1>
       {health > 1 && takingDmg && <Hitsplat x={hitsplatPosition.x} y={hitsplatPosition.y} damage={damage} />}
+      <span>{health}</span>
       <progress className="healthBar" id="health" value={health} max="78"></progress>
       <button onClick={() => !takingDmg && HandleUpdateDamage()}>
         <img src={strangeDevice} className="logo" alt="strange device" />
---
+++ src/constants.tsx
@@ -0,0 +1,12 @@
+export const geoGoal1 = {
+    latitude: 59.8604189,
+    longitude: 17.6034489
+}
+export const geoGoal2 = {
+    latitude: 59.861524,
+    longitude: 17.603079
+}
+export const geoTest = {
+    latitude: 59.860440,
+    longitude: 17.619373
+}
