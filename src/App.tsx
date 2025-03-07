359ee04da2fb358c7b874484c1311a1834ae872f
Alfradu <alfred.radu@gmail.com>
2025-03-07 08:21:45 +0000

    saving state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import strangeDevice from './assets/strange-device.png'
 import './css/App.css'
 import Hitsplat from './hitsplat.tsx'
@@ -7,28 +7,47 @@ import { geoGoal1, geoGoal2, geoTest } from './constants.tsx'
 import { headingDistanceTo, normalizeHeading } from 'geolocation-utils'
 
 function App() {
+  const initGoal: string = JSON.parse(window.sessionStorage.getItem("goal") || "");
+  const initHealth: number = parseInt(window.sessionStorage.getItem("health") || "78");
   const [geoLocation, setGeoLocation] = useState({
     latitude: 0,
     longitude: 0
   })
-  const [testing, setTesting] = useState(true);
-  const [goal, setGoal] = useState({ latitude: 0, longitude: 0 });
-  const [goalDistance, setGoalDistance] = useState({heading: 0, distance: 0});
-  const [health, setHealth] = useState(78);
-  const [takingDmg, setTakingDmg] = useState(false);
-  const [damage, setDamage] = useState(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [testing, setTesting] = useState<boolean>(true);
+  const [goal, setGoal] = useState<string>("");
+  const [goalLoc, setGoalLoc] = useState({ latitude: 0, longitude: 0});
+  const [goalDistance, setGoalDistance] = useState({ heading: 0, distance: 0 });
+  const [health, setHealth] = useState<number>(initHealth);
+  const [takingDmg, setTakingDmg] = useState<boolean>(false);
+  const [damage, setDamage] = useState<number>(0);
   const [hitsplatPosition, setHitsplatPosition] = useState({
     x: 0,
     y: 0
   });
 
   useEffect(() => {
+    setGoal(initGoal)
+  }, []);
+
+  useEffect(() => {
+    if (inputRef.current && goal != "") {
+      inputRef.current.value = goal;
+    }
+  }, [goal])
+
+  useEffect(() => {
+    window.sessionStorage.setItem("health", health.toString());
+    window.sessionStorage.setItem("goal", JSON.stringify(goal));
+  }, [health, goal]);
+
+  useEffect(() => {
     const interval = setInterval(() => {
-      if (health < 78 && !takingDmg) 
-        if (health +10 > 78) setHealth(78)
+      if (health < 78 && !takingDmg)
+        if (health + 10 > 78) setHealth(78)
         else setHealth(health + 10);
     }, 5 * 1000);
-  
+
     return () => {
       clearInterval(interval);
     };
@@ -39,8 +58,8 @@ function App() {
     if (health == 0) return;
     setTakingDmg(true);
     setDamage(currDmg);
-    if (health-currDmg < 0) setHealth(0);
-    else setHealth(health-currDmg);
+    if (health - currDmg < 0) setHealth(0);
+    else setHealth(health - currDmg);
     setHitsplatPosition({
       x: (Math.random() * 300) + window.innerWidth / 2 - 150,
       y: (Math.random() * 300) + window.innerHeight / 2 - 150
@@ -51,36 +70,36 @@ function App() {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         })
-        const headingDist = headingDistanceTo(position.coords, goal);
+        const headingDist = headingDistanceTo(position.coords, goalLoc);
         setGoalDistance(headingDist);
-      }, () => console.log("Unable to retrieve your location"), { enableHighAccuracy: true } );
+      }, () => console.log("Unable to retrieve your location"), { enableHighAccuracy: true });
     } else {
       setGeoLocation({
         latitude: geoTest.latitude,
         longitude: geoTest.longitude
       });
-      const headingDist = headingDistanceTo(geoTest, goal);
+      const headingDist = headingDistanceTo(geoTest, goalLoc);
       setGoalDistance(headingDist);
     }
     setTimeout(() => {
       setTakingDmg(false);
     }, 500);
-  }, [health, goal, testing, geoTest]);
+  }, [health, goalLoc, testing, geoTest]);
 
   const HandleUpdateGoal = useCallback(() => {
     const element = document.getElementById('input') as HTMLInputElement;
     switch (element.value.toLowerCase()) {
       case ("goal1"):
-        setGoal(geoGoal1)
+        setGoalLoc({ latitude: geoGoal1.latitude, longitude: geoGoal1.longitude});
+        setGoal("goal1");
         break;
       case ("goal2"):
-        setGoal(geoGoal2)
+        setGoalLoc({latitude: geoGoal2.latitude, longitude: geoGoal2.longitude});
+        setGoal("goal2");
         break;
       default:
-        setGoal({
-          latitude: 0,
-          longitude: 0
-        })
+        setGoalLoc({latitude: 0,longitude: 0})
+        setGoal("")
     }
   }, []);
 
@@ -89,17 +108,17 @@ function App() {
       <h2>Strange Device</h2>
       {takingDmg && <Hitsplat x={hitsplatPosition.x} y={hitsplatPosition.y} damage={damage} />}
       <progress className="healthBar" id="health" value={health} max="78"></progress>
-      <button className="deviceBtn" disabled={goal.latitude == 0 && goal.latitude == 0} onClick={() => !takingDmg && HandleUpdateDamage()}>
+      <button className="deviceBtn" disabled={goal == ""} onClick={() => !takingDmg && HandleUpdateDamage()}>
         <img src={strangeDevice} className="logo" alt="strange device" />
       </button>
       <input type="checkbox" id="testing" name="testing" checked={testing} onChange={() => setTesting(!testing)}></input>
-      <input id="input" onChange={HandleUpdateGoal}></input>
+      <input id="input" ref={inputRef} onChange={HandleUpdateGoal}></input>
       {testing &&
-        <><br/><span>pos Lat {geoLocation.latitude} ,pos Long {geoLocation.longitude}</span>
-          <br/><span>goal Lat {goal.latitude} ,goal Long {goal.longitude}</span>
-          <br/><span>Distance: {Math.floor(goalDistance.distance)} m</span>
-          <br/><span>Heading: {headingToOrientation(normalizeHeading(goalDistance.heading))}</span>
-          </>
+        <><br /><span>pos Lat {geoLocation.latitude} ,pos Long {geoLocation.longitude}</span>
+          <br /><span>goal Lat {goalLoc.latitude} ,goal Long {goalLoc.longitude}</span>
+          <br /><span>Distance: {Math.floor(goalDistance.distance)} m</span>
+          <br /><span>Heading: {headingToOrientation(normalizeHeading(goalDistance.heading))}</span>
+        </>
       }
     </>
   )
