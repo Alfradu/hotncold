a0c4d21bebaf0fcf80078c8b188d86bf36f7884b
Alfradu <alfred.radu@gmail.com>
2025-05-14 07:59:34 +0000

    added correct lat/longs

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,46 +1,61 @@
 export const objectives: Objective[] =
 	[
 		{
-			goalKeyword: "Dragon scimitar",
-			latitude: 59.8604189,
-			longitude: 17.6034489
+			goalKeyword: "Dragon scimitar", //park1 team1 step1
+			latitude: 59.862274,
+			longitude: 17.617039
 		},
 		{
-			goalKeyword: "Abyssal whip",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Abyssal whip", //park2 team2 step1
+			latitude: 59.862270,
+			longitude: 17.620841
 		},
 		{
-			goalKeyword: "Shark",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Shark", //park4 team1 step2
+			latitude: 59.860323,
+			longitude: 17.622887
 		},
 		{
-			goalKeyword: "Lobster",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Lobster", //park3 team2 step2
+			latitude: 59.861370,
+			longitude: 17.611900
 		},
 		{
-			goalKeyword: "Wyrmling bones",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Wyrmling bones", //stubbestabby team2 step5
+			latitude: 59.859910,
+			longitude: 17.601400
 		},
 		{
-			goalKeyword: "Wyrm bones",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Wyrm bones", // parkbänk botaniska team2 step4
+			latitude: 59.853120,
+			longitude: 17.628860
 		},
 		{
-			goalKeyword: "Adamant platebody",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Adamant platebody", // slottet team2 step3
+			latitude: 59.854230,
+			longitude: 17.634100
 		},
 		{
-			goalKeyword: "Bandos godsword",
-			latitude: 59.861524,
-			longitude: 17.603079
+			goalKeyword: "Bandos godsword", // domkyrkan team1 step4
+			latitude: 59.857520,
+			longitude: 17.633520
+		},
+		{
+			goalKeyword: "Bandos godsword", // höganäsparken team1 step5
+			latitude: 59.866260,
+			longitude: 17.643300
+		},
+		{
+			goalKeyword: "Bandos godsword", // semijnarieparken team1 step3
+			latitude: 59.867660,
+			longitude: 17.621580
 		}
 	];
+//todo: add dummy items that look similar to harder steps
+//		something like range pot looking like prayer pot?
+// 		steps 3-5 should have one or two duds ea.
+//		lat/long can be something stupid far away handled with a
+//		special text indicating that its definitely wrong
 
 type Objective = {
 	goalKeyword: string;
